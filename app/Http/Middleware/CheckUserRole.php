02be6310927e88c896f58b9152a9948559476741
prithirabha <127822494+prithirabha@users.noreply.github.com>
2023-11-29 21:56:38 +0530

    Admin now has access to any page

--- app/Http/Middleware/CheckUserRole.php
+++ app/Http/Middleware/CheckUserRole.php
@@ -12,6 +12,10 @@ class CheckUserRole
         $user = auth()->user();
 
         // Check if the user's role matches the expected role
+        if ($user && $user->role_id === 1) {
+            return $next($request);
+        }
+
         if ($user && $user->role_id != $role) {
             return redirect()->route('404');
         }
